bbf91886fc4a799851606ffb2f9967b969359b05
Garry McGlennon <garrm@microsoft.com>
2015-05-29 21:20:20 -0700

    adding missing files

---
+++ ABus/Config/Transport/TransportGrammar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection.Emit;
+using ABus.Contracts;
+using Microsoft.Practices.Unity;
+
+namespace ABus.Config.Transport
+{
+    public class TransportDefinitionGrammar
+    {
+        ConfigurationGrammar Parent { get; set; }
+        TransportDefinition Definition { get; set; }
+
+        public TransportDefinitionGrammar(ConfigurationGrammar parent, TransportDefinition definition)
+        {
+            this.Parent = parent;
+            this.Definition = definition;
+        }
+
+        public ConfigurationGrammar And()
+        {
+            return this.Parent;
+        }
+        
+        public TransportDefinitionGrammar WithConnectionString(string connectionstring)
+        {
+            // TODO: Add more validation
+
+            // The connection string format is a complex object so needs to be broken apart
+            var parts = connectionstring.Split(';');
+            if (parts.Length != 3)
+                throw new ArgumentException("Transport connection string is invalid: " + connectionstring);
+
+            this.Definition.Uri = parts[0].Substring(9);
+            this.Definition.Credentials = string.Format("{0};{1}", parts[1], parts[2]);
+
+            return this;
+        }
+    }
+}
