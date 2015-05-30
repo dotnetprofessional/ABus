4a57b4670e1d66c532e03eac98fb7900d06f82e1
dnp <dnp@dotnetprofessional.com>
2015-05-29 23:07:36 -0700

    Adding missing files

---
+++ ABus/Config/Transactions/TransactionsGrammar.cs
@@ -0,0 +1,27 @@
+﻿namespace ABus.Config.Transactions
+{
+    public class TransactionsGrammar
+    {
+        internal ConfigurationGrammar Parent { get; set; }
+
+        public TransactionsGrammar(ConfigurationGrammar parent)
+        {
+            this.Parent = parent;
+        }
+
+        public TransactionsGrammar WithTransactionManager<T>(string connectionstring)
+        {
+            var tm = typeof (T);
+            this.Parent.Configuration.Transactions.TransactionManagerType = tm.AssemblyQualifiedName;
+            this.Parent.Configuration.Transactions.TransactionManagerConnectionString = connectionstring;
+
+            return this;
+        }
+
+        public TransactionsGrammar Disable()
+        {
+            this.Parent.Configuration.Transactions.TransactionsEnabled = false;
+            return this;
+        }
+    }
+}
